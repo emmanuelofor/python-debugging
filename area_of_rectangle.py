61ca49726ba184a6e8fecbceb096ebf12c4fa9dc
emmanuelofor <emmanuel.ofori@kibo.school>
2023-02-22 21:22:04 +0000

    area_of rectangle_Exercise

--- area_of_rectangle.py
+++ area_of_rectangle.py
@@ -5,7 +5,7 @@
 import sys
 
 
-def area_of_rectangle(height, width = None):
+def area_of_rectangle(height, width=None):
     """
     Returns the area of a rectangle.
 
@@ -13,8 +13,8 @@ def area_of_rectangle(height, width = None):
     ----------
     height : int or float 
         The height of the rectangle.
-    width : int or float
-        The width of the rectangle. If `None` width is assumed to be equal to 
+    width : int or float, optional
+        The width of the rectangle. If `None`, width is assumed to be equal to 
         the height.
 
     Returns
@@ -26,30 +26,31 @@ def area_of_rectangle(height, width = None):
     --------
     >>> area_of_rectangle(7)
     49
-    >>> area_of_rectangle (7, 2)
+    >>> area_of_rectangle(7, 2)
     14
     """
-    if width:
+    if width is None:
         width = height
     area = height * width
     return area
 
+
 if __name__ == '__main__':
-    if (len(sys.argv) < 2) or (len(sys.argv) > 3):
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
         message = (
-                "{script_name}: Expecting one or two command-line arguments:\n"
-                "\tthe height of a square or the height and width of a "
-                "rectangle".format(script_name = sys.argv[0]))
+            "{script_name}: Expecting one or two command-line arguments:\n"
+            "\tthe height of a square or the height and width of a "
+            "rectangle".format(script_name=sys.argv[0]))
         sys.exit(message)
-    height = sys.argv[1]
-    width = height
-    if len(sys.argv) > 3:
-        width = sys.argv[1]
+
+    height = float(sys.argv[1])
+    width = float(sys.argv[2]) if len(sys.argv) == 3 else None
 
     area = area_of_rectangle(height, width)
 
     message = "The area of a {h} X {w} rectangle is {a}".format(
-            h = height,
-            w = width,
-            a = area)
+        h=height,
+        w=width or height,
+        a=area)
     print(message)
+
